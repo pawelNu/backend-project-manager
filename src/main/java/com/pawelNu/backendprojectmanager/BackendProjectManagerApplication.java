3ea3c566bfde3daa4c4b370e54d86b9cb8fabede
pawelNu <you@example.com>
2023-09-19 17:14:15 +0200

    First commit

---
+++ src/main/java/com/pawelNu/backendprojectmanager/BackendProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pawelNu.backendprojectmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawelNu/backendprojectmanager/BackendProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelNu.backendprojectmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
