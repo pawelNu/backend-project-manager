0edb58c5ff15ffc85dc261f7e0765b3d41f6e172
pawelNu <93542936+pawelNu@users.noreply.github.com>
2023-11-19 11:10:11 +0100

    feat (3): added spring boot starter code (#4)

---
+++ src/main/java/com/pawelnu/BackendProjectManager/BackendProjectManagerApplication.java
@@ -0,0 +1,13 @@
+package com.pawelnu.BackendProjectManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendProjectManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendProjectManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pawelnu/BackendProjectManager/BackendProjectManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pawelnu.BackendProjectManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendProjectManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
