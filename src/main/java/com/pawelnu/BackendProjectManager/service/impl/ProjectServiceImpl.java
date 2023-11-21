bf83ebd778c6089095030e9315274fffd6c7ce8d
pawelNu <93542936+pawelNu@users.noreply.github.com>
2023-11-21 18:49:41 +0100

    feat (24): added get project by id (#25)

--- src/main/java/com/pawelnu/BackendProjectManager/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/pawelnu/BackendProjectManager/service/impl/ProjectServiceImpl.java
@@ -2,14 +2,16 @@ package com.pawelnu.BackendProjectManager.service.impl;
 
 import com.pawelnu.BackendProjectManager.dto.ProjectDTO;
 import com.pawelnu.BackendProjectManager.entity.ProjectEntity;
+import com.pawelnu.BackendProjectManager.exception.NotFoundException;
 import com.pawelnu.BackendProjectManager.mapper.ProjectMapper;
 import com.pawelnu.BackendProjectManager.repository.ProjectRepository;
 import com.pawelnu.BackendProjectManager.service.IProjectService;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -21,9 +23,16 @@ public class ProjectServiceImpl implements IProjectService {
     @Override
     public List<ProjectDTO> getAllProjects() {
         List<ProjectEntity> projectEntities = projectRepository.findAll();
-        return projectEntities.stream()
-                .map(projectMapper::toDTO)
-                .collect(Collectors.toList());
+        return projectEntities.stream().map(projectMapper::toDTO).collect(Collectors.toList());
     }
 
+    @Override
+    public ProjectDTO getProjectById(UUID projectId) {
+        Optional<ProjectEntity> projectById = projectRepository.findById(projectId);
+        if (projectById.isPresent()) {
+            return projectMapper.toDTO(projectById.get());
+        } else {
+            throw new NotFoundException("Project not found with id: " + projectId);
+        }
+    }
 }
