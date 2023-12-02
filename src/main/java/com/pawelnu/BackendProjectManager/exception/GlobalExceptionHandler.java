fdb1aae8a7f3286718238fcae687709e9963334e
pawelNu <pnurzynski17@gmail.com>
2023-12-02 14:05:50 +0100

    refactor (37): code refactor

--- src/main/java/com/pawelnu/BackendProjectManager/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pawelnu/BackendProjectManager/exception/GlobalExceptionHandler.java
@@ -4,14 +4,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(BadRequestException.class)
-    public ResponseEntity<String> handleBadRequestException(
-            BadRequestException e) {
+    public ResponseEntity<String> handleBadRequestException(BadRequestException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
@@ -19,5 +17,4 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
     }
-
 }
