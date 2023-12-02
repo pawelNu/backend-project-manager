5f874e5ce85bbfd0420f0c39efac863b5c0d1500
pawelNu <pnurzynski17@gmail.com>
2023-12-02 13:53:22 +0100

    fix (38): added GlobalExceptionHandler, now BadRequestException returns: Unsupported project status: value

---
+++ src/main/java/com/pawelnu/BackendProjectManager/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.pawelnu.BackendProjectManager.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<String> handleBadRequestException(
+            BadRequestException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
