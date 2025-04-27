21290b9cf725eea2bf45774c25ed6c8fbe1d56ff
pawelNu <pnurzynski17@gmail.com>
2025-04-27 10:14:11 +0200

    feat: added company address repository

---
+++ src/main/java/com/pawelnu/projectmanager/endpoints/companyaddress/CompanyAddressRepository.java
@@ -0,0 +1,11 @@
+package com.pawelnu.projectmanager.endpoints.companyaddress;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface CompanyAddressRepository
+    extends JpaRepository<CompanyAddressEntity, UUID>,
+    QuerydslPredicateExecutor<CompanyAddressEntity> {
+
+}
