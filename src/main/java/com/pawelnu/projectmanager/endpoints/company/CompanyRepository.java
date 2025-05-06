7d0c039999e5261cb16701ef98994596fa7ed870
pawelNu <pnurzynski17@gmail.com>
2025-05-06 10:49:17 +0200

    refactor: removed unnecessary methods override

--- src/main/java/com/pawelnu/projectmanager/endpoints/company/CompanyRepository.java
+++ src/main/java/com/pawelnu/projectmanager/endpoints/company/CompanyRepository.java
@@ -1,14 +1,8 @@
 package com.pawelnu.projectmanager.endpoints.company;
 
-import com.querydsl.core.types.Predicate;
 import java.util.UUID;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface CompanyRepository
-    extends JpaRepository<CompanyEntity, UUID>, QuerydslPredicateExecutor<CompanyEntity> {
-
-  Page<CompanyEntity> findAll(Predicate predicate, Pageable pageable);
-}
+    extends JpaRepository<CompanyEntity, UUID>, QuerydslPredicateExecutor<CompanyEntity> {}
