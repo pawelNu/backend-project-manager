934db2cc5107bdef0ce4cbdf5672ec89954ba69a
pawelNu <93542936+pawelNu@users.noreply.github.com>
2025-05-10 16:28:32 +0200

    75 feat prepare liquibase for employee (#76)
    * docs: added liquibase script for employees
    
    * docs: added liquibase script for employees
    
    * docs: added liquibase script for employees

---
+++ src/main/resources/db/changelog/0.4.0/empoloyee/create_employees_table.sql
@@ -0,0 +1,21 @@
+create table employees (
+id uuid not null,
+created timestamp(6) without time zone,
+last_modified timestamp(6) without time zone,
+version integer,
+email varchar(255) not null,
+first_name varchar(255) not null,
+last_name varchar(255) not null,
+password varchar(255) not null,
+phone_number varchar(255) not null,
+username varchar(255) not null,
+company_id uuid not null,
+primary key (id)
+)
+;
+
+alter table if exists employees
+add constraint fk_employees_and_companies
+foreign key (company_id)
+references companies
+;
