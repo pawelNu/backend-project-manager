ae4ffc0b7f6a1e38ca9fbe200c820dd252e6ae7e
pawelNu <pnurzynski17@gmail.com>
2025-05-11 12:33:14 +0200

    refactor: removed comments

--- src/main/resources/db/add_uuid_to_changes.py
+++ src/main/resources/db/add_uuid_to_changes.py
@@ -5,13 +5,11 @@ def replace_all_ids_with_fresh_sequence(root_dir):
   current_id = 1
   yaml_file_paths = []
 
-  # Zbieramy ścieżki do wszystkich .yaml
   for dirpath, _, filenames in os.walk(root_dir):
     for filename in filenames:
       if filename.endswith(".yaml"):
         yaml_file_paths.append(os.path.join(dirpath, filename))
 
-  # Iterujemy po plikach w stabilnej kolejności (dla powtarzalności)
   for filepath in sorted(yaml_file_paths):
     with open(filepath, "r", encoding="utf-8") as file:
       lines = file.readlines()
