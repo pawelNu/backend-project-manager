bb0d4943de0db5a28eef825373e4bf2f102fb4dd
pawelNu <pnurzynski17@gmail.com>
2025-05-13 06:58:30 +0200

    refactor: added field isDeleted

--- src/main/resources/db/changelog/0.5.0/authorities/create_authorities_table.sql
+++ src/main/resources/db/changelog/0.5.0/authorities/create_authorities_table.sql
@@ -4,6 +4,7 @@ created timestamp(6) without time zone,
 last_modified timestamp(6) without time zone,
 version integer,
 name varchar(255) not null,
+is_deleted boolean not null,
 primary key (id)
 )
 ;
