21ba5a392e112005ffcec81095ef81b191de8bd5
pawelNu <pnurzynski17@gmail.com>
2025-05-20 13:48:56 +0200

    feat: added script create_table_employee_authorities.sql

---
+++ src/main/resources/db/changelog/0.2.0/create_table_employee_authorities.sql
@@ -0,0 +1,14 @@
+create table employee_authorities (
+    id uuid not null,
+    created timestamp(6) without time zone not null,
+    last_modified timestamp(6) without time zone not null,
+    version integer not null,
+    authority_id uuid,
+    employee_id uuid,
+    is_deleted boolean not null,
+    primary key (id)
+);
+alter table if exists employee_authorities
+add constraint fk_employee_authorities_and_authorities foreign key (authority_id) references authorities;
+alter table if exists employee_authorities
+add constraint fk_employee_authorities_and_employees foreign key (employee_id) references employees;
