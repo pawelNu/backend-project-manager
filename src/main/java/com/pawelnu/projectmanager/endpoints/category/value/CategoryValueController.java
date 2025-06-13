e9a7ba534cd1f6d693e2310560e15ef84a556fd2
pawelNu <pnurzynski17@gmail.com>
2025-06-13 09:59:20 +0200

    feat: added category value classes

--- src/main/java/com/pawelnu/projectmanager/endpoints/category/value/CategoryValueController.java
+++ src/main/java/com/pawelnu/projectmanager/endpoints/category/value/CategoryValueController.java
@@ -26,24 +26,24 @@ public class CategoryValueController implements CategoryValueApi {
     //    return ResponseEntity.ok()
     //        .header("Content-Range", result.getContentRange())
     //        .body(result.getData());
-      throw new UnsupportedOperationException("Not implemented yet");
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public ResponseEntity<CategoryValueDTO> getById(UUID id) {
     //    return ResponseEntity.ok(categoryValueService.getById(id));
-      throw new UnsupportedOperationException("Not implemented yet");
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public ResponseEntity<CategoryValueDTO> editById(UUID id, CategoryValueEditRequestDTO body) {
     //    return ResponseEntity.ok(categoryValueService.editById(id, body));
-      throw new UnsupportedOperationException("Not implemented yet");
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 
   @Override
   public ResponseEntity<SimpleResponse> deleteById(UUID id) {
     //    return ResponseEntity.ok(categoryValueService.deleteById(id));
-      throw new UnsupportedOperationException("Not implemented yet");
+    throw new UnsupportedOperationException("Not implemented yet");
   }
 }
