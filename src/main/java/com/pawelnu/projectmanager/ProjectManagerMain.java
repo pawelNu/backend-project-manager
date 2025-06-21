78d8bfbc7e3ebfba023135b2e970d07c36599505
pawelNu <pnurzynski17@gmail.com>
2025-06-21 17:58:23 +0200

    docs: added TODOs

--- src/main/java/com/pawelnu/projectmanager/ProjectManagerMain.java
+++ src/main/java/com/pawelnu/projectmanager/ProjectManagerMain.java
@@ -14,7 +14,6 @@ public class ProjectManagerMain {
   }
 }
 
-// TODO api for category_values
 // TODO api for companies
 // TODO api for company_addresses
 // TODO api for employees
---
+++ src/test/java/com/pawelnu/projectmanager/endpoints/category/value/CategoryValueControllerTest.java
@@ -0,0 +1,16 @@
+package com.pawelnu.projectmanager.endpoints.category.value;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CategoryValueControllerTest {
+
+  //  TODO Test void create()
+
+  //  TODO Test void getList()
+
+  //  TODO Test void getById()
+
+  //  TODO Test void editById()
+
+  //  TODO Test void deleteById()
+}
