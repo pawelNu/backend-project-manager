c53151300073fa1fdea566ce3d42cece7c5cd5be
pawelNu <pnurzynski17@gmail.com>
2025-06-22 08:52:42 +0200

    test: added test shouldReturn_200_getCompanyAddressList

--- src/test/java/com/pawelnu/projectmanager/endpoints/company/address/CompanyAddressControllerTest.java
+++ src/test/java/com/pawelnu/projectmanager/endpoints/company/address/CompanyAddressControllerTest.java
@@ -5,14 +5,19 @@ import static com.pawelnu.projectmanager.utils.Utils.accessDeniedError;
 import static com.pawelnu.projectmanager.utils.Utils.withBadJwt;
 import static com.pawelnu.projectmanager.utils.Utils.withJwt;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pawelnu.projectmanager.config.security.jwt.JwtUtils;
+import com.pawelnu.projectmanager.endpoints.category.CategoryDTO;
 import com.pawelnu.projectmanager.exception.model.ReactAdminBadRequestError;
 import com.pawelnu.projectmanager.exception.model.ReactAdminError;
 import com.pawelnu.projectmanager.utils.Path;
 import com.pawelnu.projectmanager.utils.Utils;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
@@ -188,7 +193,121 @@ class CompanyAddressControllerTest {
     assertEquals(accessDeniedError(), responseBody);
   }
 
-  // TODO Test getList() {}
+  @Test
+  void shouldReturn_200_getCompanyAddressList() throws Exception {
+    List<String> range = List.of("0", "1");
+    String rangeString = objectMapper.writeValueAsString(range);
+    MvcResult response =
+        mockMvc.perform(get(BASE_URL).with(withJwt()).param("range", rangeString)).andReturn();
+    int status = response.getResponse().getStatus();
+    String headerContentRange = response.getResponse().getHeader("Content-Range");
+    String contentAsString = response.getResponse().getContentAsString();
+    List<CompanyAddressDTO> responseBody =
+        objectMapper.readValue(contentAsString, new TypeReference<>() {});
+    assertEquals(HttpStatus.OK.value(), status);
+    assertEquals("items 0-1", headerContentRange.substring(0, 9));
+    assertEquals(2, responseBody.size());
+  }
+
+  @Test
+  void shouldReturn_200_getCompanyAddressList_withFilters() throws Exception {
+    Map<String, String> filter = Map.of("name", "role");
+    String filterStrig = objectMapper.writeValueAsString(filter);
+    MvcResult response =
+        mockMvc.perform(get(BASE_URL).with(withJwt()).param("filter", filterStrig)).andReturn();
+    int status = response.getResponse().getStatus();
+    String headerContentRange = response.getResponse().getHeader("Content-Range");
+    String contentAsString = response.getResponse().getContentAsString();
+    List<CategoryDTO> responseBody =
+        objectMapper.readValue(contentAsString, new TypeReference<>() {});
+    assertEquals(HttpStatus.OK.value(), status);
+    assertEquals("items 0-0/1", headerContentRange);
+    assertEquals(1, responseBody.size());
+    assertEquals("employee role", responseBody.getFirst().getName());
+  }
+
+  @Test
+  void shouldReturn_200_getCompanyAddressList_withFiltersAndSort() throws Exception {
+    List<String> sort = List.of("name", "DESC");
+    Map<String, String> filter = Map.of("name", "status");
+    String sortString = objectMapper.writeValueAsString(sort);
+    String filterStrig = objectMapper.writeValueAsString(filter);
+    MvcResult response =
+        mockMvc
+            .perform(
+                get(BASE_URL)
+                    .with(withJwt())
+                    .param("sort", sortString)
+                    .param("filter", filterStrig))
+            .andReturn();
+    int status = response.getResponse().getStatus();
+    String headerContentRange = response.getResponse().getHeader("Content-Range");
+    String contentAsString = response.getResponse().getContentAsString();
+    List<CategoryDTO> responseBody =
+        objectMapper.readValue(contentAsString, new TypeReference<>() {});
+    assertEquals(HttpStatus.OK.value(), status);
+    assertEquals("items 0-1/2", headerContentRange);
+    assertEquals(2, responseBody.size());
+    assertEquals("company status 1", responseBody.getFirst().getName());
+  }
+
+  @Test
+  void shouldReturn_200_getCompanyAddressList_withRange() throws Exception {
+    List<String> range = List.of("0", "0");
+    List<String> sort = List.of("name", "ASC");
+    String rangeString = objectMapper.writeValueAsString(range);
+    String sortString = objectMapper.writeValueAsString(sort);
+    MvcResult response =
+        mockMvc
+            .perform(
+                get(BASE_URL).with(withJwt()).param("sort", sortString).param("range", rangeString))
+            .andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    List<CategoryDTO> responseBody =
+        objectMapper.readValue(contentAsString, new TypeReference<>() {});
+    assertEquals(HttpStatus.OK.value(), status);
+    assertEquals(1, responseBody.size());
+    assertEquals("category a", responseBody.getFirst().getName());
+  }
+
+  @Test
+  void shouldReturn_200_getCompanyAddressList_emptyResult() throws Exception {
+    Map<String, String> filter = Map.of("name", "not exists");
+    String filterStrig = objectMapper.writeValueAsString(filter);
+    MvcResult response =
+        mockMvc.perform(get(BASE_URL).with(withJwt()).param("filter", filterStrig)).andReturn();
+    int status = response.getResponse().getStatus();
+    String headerContentRange = response.getResponse().getHeader("Content-Range");
+    String contentAsString = response.getResponse().getContentAsString();
+    List<CategoryDTO> responseBody =
+        objectMapper.readValue(contentAsString, new TypeReference<>() {});
+    assertEquals(HttpStatus.OK.value(), status);
+    assertEquals("items 0--1/0", headerContentRange);
+    assertEquals(0, responseBody.size());
+  }
+
+  @Test
+  void shouldReturn_401_getCompanyAddressList() throws Exception {
+    MvcResult response = mockMvc.perform(get(BASE_URL)).andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    ReactAdminError responseBody = objectMapper.readValue(contentAsString, ReactAdminError.class);
+    assertEquals(HttpStatus.UNAUTHORIZED.value(), status);
+    ReactAdminError expectedResponse =
+        new ReactAdminError("Full authentication is required to access this resource");
+    assertEquals(expectedResponse, responseBody);
+  }
+
+  @Test
+  void shouldReturn_403_getCompanyAddressList() throws Exception {
+    MvcResult response = mockMvc.perform(get(BASE_URL).with(withBadJwt())).andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    ReactAdminError responseBody = objectMapper.readValue(contentAsString, ReactAdminError.class);
+    assertEquals(HttpStatus.FORBIDDEN.value(), status);
+    assertEquals(Utils.accessDeniedError(), responseBody);
+  }
 
   // TODO Test getById() {}
 
