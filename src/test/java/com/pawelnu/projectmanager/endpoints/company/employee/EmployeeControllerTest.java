d1fa6b2d9d7bddc5adfb6fc62bb47ce67e0edd2f
pawelNu <pnurzynski17@gmail.com>
2025-06-23 08:21:47 +0200

    test: added tests createEmployee

--- src/test/java/com/pawelnu/projectmanager/endpoints/company/employee/EmployeeControllerTest.java
+++ src/test/java/com/pawelnu/projectmanager/endpoints/company/employee/EmployeeControllerTest.java
@@ -1,20 +1,32 @@
 package com.pawelnu.projectmanager.endpoints.company.employee;
 
+import static com.pawelnu.projectmanager.utils.Utils.FULL_AUTH_IS_REQUIRED;
+import static com.pawelnu.projectmanager.utils.Utils.accessDeniedError;
+import static com.pawelnu.projectmanager.utils.Utils.withBadJwt;
+import static com.pawelnu.projectmanager.utils.Utils.withJwt;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pawelnu.projectmanager.config.security.jwt.JwtUtils;
+import com.pawelnu.projectmanager.exception.model.ReactAdminBadRequestError;
+import com.pawelnu.projectmanager.exception.model.ReactAdminError;
 import com.pawelnu.projectmanager.utils.Path;
 import com.pawelnu.projectmanager.utils.Utils;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
@@ -48,7 +60,120 @@ class EmployeeControllerTest {
   void beforeEach() {
     Utils.generateToken(jwtUtils);
   }
-  //  TODO Test create()
+
+  @Test
+  void shouldReturn_201_createEmployee() throws Exception {
+    EmployeeCreateRequestDTO request =
+        EmployeeCreateRequestDTO.builder()
+            .companyId(UUID.fromString("cf578fec-006b-4604-a5e8-5ad1b3ea2be5"))
+            .firstName("test firstName")
+            .lastName("test lastName")
+            .username("testUsername")
+            .password("testPassword")
+            .email("test.test@test.test")
+            .phoneNumber("111-111-111")
+            .build();
+    String requestBody = objectMapper.writeValueAsString(request);
+    MvcResult response =
+        mockMvc
+            .perform(
+                post(BASE_URL)
+                    .with(withJwt())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+            .andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    EmployeeDTO responseBody = objectMapper.readValue(contentAsString, EmployeeDTO.class);
+    assertEquals(HttpStatus.CREATED.value(), status);
+    assertEquals("Hayes-Welch", responseBody.getCompanyName());
+    assertEquals(request.getFirstName(), responseBody.getFirstName());
+    assertEquals(request.getLastName(), responseBody.getLastName());
+    assertEquals(request.getUsername(), responseBody.getUsername());
+    assertEquals(request.getEmail(), responseBody.getEmail());
+    assertEquals(request.getPhoneNumber(), responseBody.getPhoneNumber());
+  }
+
+  @Test
+  void shouldReturn_400_createEmployee() throws Exception {
+    EmployeeCreateRequestDTO request =
+        EmployeeCreateRequestDTO.builder()
+            .companyId(UUID.fromString("cf578fec-006b-4604-a5e8-5ad1b3ea2be5"))
+            .firstName("test firstName")
+            .lastName("test lastName")
+            .username("testUsername")
+            .password("testPassword")
+            .email("invalid.email")
+            .phoneNumber("111-111-111")
+            .build();
+    String requestBody = objectMapper.writeValueAsString(request);
+    MvcResult response =
+        mockMvc
+            .perform(
+                post(BASE_URL)
+                    .with(withJwt())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+            .andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    ReactAdminBadRequestError responseBody =
+        objectMapper.readValue(contentAsString, ReactAdminBadRequestError.class);
+    assertEquals(HttpStatus.BAD_REQUEST.value(), status);
+    assertEquals("must be a well-formed email address", responseBody.getErrors().get("email"));
+  }
+
+  @Test
+  void shouldReturn_401_createEmployee() throws Exception {
+    EmployeeCreateRequestDTO request =
+        EmployeeCreateRequestDTO.builder()
+            .companyId(UUID.fromString("cf578fec-006b-4604-a5e8-5ad1b3ea2be5"))
+            .firstName("test firstName")
+            .lastName("test lastName")
+            .username("testUsername")
+            .password("testPassword")
+            .email("test.test@test.test")
+            .phoneNumber("111-111-111")
+            .build();
+    String requestBody = objectMapper.writeValueAsString(request);
+    MvcResult response =
+        mockMvc
+            .perform(post(BASE_URL).contentType(MediaType.APPLICATION_JSON).content(requestBody))
+            .andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    ReactAdminError responseBody = objectMapper.readValue(contentAsString, ReactAdminError.class);
+    assertEquals(HttpStatus.UNAUTHORIZED.value(), status);
+    assertEquals(FULL_AUTH_IS_REQUIRED, responseBody.getMessage());
+  }
+
+  @Test
+  void shouldReturn_403_createEmployee() throws Exception {
+    EmployeeCreateRequestDTO request =
+        EmployeeCreateRequestDTO.builder()
+            .companyId(UUID.fromString("cf578fec-006b-4604-a5e8-5ad1b3ea2be5"))
+            .firstName("test firstName")
+            .lastName("test lastName")
+            .username("testUsername")
+            .password("testPassword")
+            .email("test.test@test.test")
+            .phoneNumber("111-111-111")
+            .build();
+    String requestBody = objectMapper.writeValueAsString(request);
+    MvcResult response =
+        mockMvc
+            .perform(
+                post(BASE_URL)
+                    .with(withBadJwt())
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(requestBody))
+            .andReturn();
+    int status = response.getResponse().getStatus();
+    String contentAsString = response.getResponse().getContentAsString();
+    ReactAdminError responseBody = objectMapper.readValue(contentAsString, ReactAdminError.class);
+    assertEquals(HttpStatus.FORBIDDEN.value(), status);
+    assertEquals(accessDeniedError(), responseBody);
+  }
 
   //  TODO Test getList()
 
