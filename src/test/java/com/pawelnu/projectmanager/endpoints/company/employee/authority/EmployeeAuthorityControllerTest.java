4b477783075f12d8edc497fb06d93a1acca3da77
pawelNu <pnurzynski17@gmail.com>
2025-06-27 17:02:57 +0200

    test: added EmployeeAuthorityControllerTest

---
+++ src/test/java/com/pawelnu/projectmanager/endpoints/company/employee/authority/EmployeeAuthorityControllerTest.java
@@ -0,0 +1,13 @@
+package com.pawelnu.projectmanager.endpoints.company.employee.authority;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeAuthorityControllerTest {
+
+  //  TODO add Test create() {
+
+  //  TODO add Test getList() {
+
+  //  TODO add Test delete() {
+
+}
