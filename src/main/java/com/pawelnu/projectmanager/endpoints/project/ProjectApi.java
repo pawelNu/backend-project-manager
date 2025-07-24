a6b43ec90d7ce8a0abc91342c69b20f0cfbd9751
pawelNu <pnurzynski17@gmail.com>
2025-07-24 19:40:21 +0200

    refactor: refactored endpoints descriptions

--- src/main/java/com/pawelnu/projectmanager/endpoints/project/ProjectApi.java
+++ src/main/java/com/pawelnu/projectmanager/endpoints/project/ProjectApi.java
@@ -42,7 +42,7 @@ public interface ProjectApi {
   ResponseEntity<ProjectDTO> create(@Valid @RequestBody ProjectCreateRequestDTO body);
 
   @Operation(
-      description = "List companies with filtering, sorting and pagination (react-admin format)")
+      description = "List projects with filtering, sorting and pagination (react-admin format)")
   @ApiResponse(
       responseCode = "200",
       description = "OK",
@@ -72,7 +72,7 @@ public interface ProjectApi {
       })
   @ResponseErrors
   @GetMapping("/{id}")
-  @Operation(description = "Get company by id.")
+  @Operation(description = "Get project by id.")
   ResponseEntity<ProjectDTO> getById(@Parameter() @PathVariable() UUID id);
 
   @ApiResponse(
@@ -85,7 +85,7 @@ public interface ProjectApi {
       })
   @ResponseErrors
   @PutMapping("/{id}")
-  @Operation(description = "Edit company by id.")
+  @Operation(description = "Edit project by id.")
   ResponseEntity<ProjectDTO> editById(
       @Parameter() @PathVariable() UUID id, @Valid @RequestBody ProjectEditRequestDTO body);
 
@@ -99,6 +99,6 @@ public interface ProjectApi {
       })
   @ResponseErrors
   @DeleteMapping("/{id}")
-  @Operation(description = "Delete company by id.")
+  @Operation(description = "Delete project by id.")
   ResponseEntity<SimpleResponse> deleteById(@Parameter() @PathVariable() UUID id);
 }
