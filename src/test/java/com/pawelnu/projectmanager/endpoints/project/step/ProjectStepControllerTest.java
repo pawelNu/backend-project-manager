54a10c817c994bb239e4f8150df5c71048230041
pawelNu <pnurzynski17@gmail.com>
2025-07-30 10:09:39 +0200

    fix: fixed ProjectStepQueryRepository

--- src/test/java/com/pawelnu/projectmanager/endpoints/project/step/ProjectStepControllerTest.java
+++ src/test/java/com/pawelnu/projectmanager/endpoints/project/step/ProjectStepControllerTest.java
@@ -247,7 +247,7 @@ class ProjectStepControllerTest {
     List<ProjectStepDTO> responseBody =
         objectMapper.readValue(contentAsString, new TypeReference<>() {});
     assertEquals(HttpStatus.OK.value(), status);
-    assertEquals("items 0-3/4", headerContentRange);//FIXME test
+    assertEquals("items 0-3/4", headerContentRange); // FIXME test
     assertEquals(4, responseBody.size());
     assertEquals("step 3", responseBody.getFirst().getName());
   }
