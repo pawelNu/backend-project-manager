d67e2d74f78bf1e4a2c9b38fd8bab47d8b096d29
pawelNu <pnurzynski17@gmail.com>
2025-08-08 12:54:44 +0200

    style: code format

--- src/main/java/com/pawelnu/projectmanager/endpoints/project/step/comment/ProjectStepCommentQueryRepository.java
+++ src/main/java/com/pawelnu/projectmanager/endpoints/project/step/comment/ProjectStepCommentQueryRepository.java
@@ -101,17 +101,16 @@ public class ProjectStepCommentQueryRepository {
       JPAQuery<ProjectStepCommentRowDTO> query, String sortField, String sortDir) {
     Order order = Sort.Direction.fromString(sortDir) == Sort.Direction.ASC ? Order.ASC : Order.DESC;
     switch (sortField) {
-      case Field.created ->
-          query.orderBy(
-              order == Order.ASC
-                  ? projectStepComment.created.asc()
-                  : projectStepComment.created.desc());
-      case Field.projectStepName ->
-          query.orderBy(order == Order.ASC ? projectStep.name.asc() : projectStep.name.desc());
-      case Field.projectName ->
-          query.orderBy(order == Order.ASC ? project.name.asc() : project.name.desc());
-      case Field.assignedEmployee ->
-          query.orderBy(order == Order.ASC ? employee.lastName.asc() : employee.lastName.desc());
+      case Field.created -> query.orderBy(
+          order == Order.ASC
+              ? projectStepComment.created.asc()
+              : projectStepComment.created.desc());
+      case Field.projectStepName -> query.orderBy(
+          order == Order.ASC ? projectStep.name.asc() : projectStep.name.desc());
+      case Field.projectName -> query.orderBy(
+          order == Order.ASC ? project.name.asc() : project.name.desc());
+      case Field.assignedEmployee -> query.orderBy(
+          order == Order.ASC ? employee.lastName.asc() : employee.lastName.desc());
       default -> query.orderBy(projectStepComment.created.asc());
     }
   }
