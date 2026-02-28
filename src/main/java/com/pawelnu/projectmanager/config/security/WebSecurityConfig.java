98aeb5d5adbcf62f81c26144f2443bc3336b9ab8
pawelNu <pnurzynski17@gmail.com>
2026-02-28 19:50:44 +0100

    refactor: refactored authenticationProvider

--- src/main/java/com/pawelnu/projectmanager/config/security/WebSecurityConfig.java
+++ src/main/java/com/pawelnu/projectmanager/config/security/WebSecurityConfig.java
@@ -47,11 +47,8 @@ public class WebSecurityConfig {
 
   @Bean
   public DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-
-    authProvider.setUserDetailsService(userDetailsService);
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(userDetailsService);
     authProvider.setPasswordEncoder(passwordEncoder);
-
     return authProvider;
   }
 
